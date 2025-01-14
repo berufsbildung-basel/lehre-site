ffac29ee96db34054de588813ded0ce71378492d
Tristan Thomas <tristan.thomas@bbzbl-it.ch>
2025-01-14 08:36:19 +0100

    readded everything

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,21 +13,21 @@
 /**
  * The decision engine for where to get Milo's libs from.
  */
-// export const [setLibs, getLibs] = (() => {
-//   let libs;
-//   return [
-//     (prodLibs, location) => {
-//       libs = (() => {
-//         const { hostname, search } = location || window.location;
-//         if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
-//         const branch = new URLSearchParams(search).get('milolibs') || 'main';
-//         if (branch === 'local') return 'http://localhost:6456/libs';
-//         return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
-//       })();
-//       return libs;
-//     }, () => libs,
-//   ];
-// })();
+export const [setLibs, getLibs] = (() => {
+  let libs;
+  return [
+    (prodLibs, location) => {
+      libs = (() => {
+        const { hostname, search } = location || window.location;
+        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
+        const branch = new URLSearchParams(search).get('milolibs') || 'main';
+        if (branch === 'local') return 'http://localhost:6456/libs';
+        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
+      })();
+      return libs;
+    }, () => libs,
+  ];
+})();
 
 /*
  * ------------------------------------------------------------
