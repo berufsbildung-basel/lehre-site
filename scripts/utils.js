9235fa408293656865dd47aa9058c1b842cbc209
Tristan Thomas <tristan.thomas@bbzbl-it.ch>
2025-01-14 15:41:03 +0100

    changed route

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,19 +13,23 @@
 /**
  * The decision engine for where to get Milo's libs from.
  */
-export const [setLibs, getLibs] = (() => {
+ export const [setLibs, getLibs] = (() => {
   let libs;
   return [
     (prodLibs, location) => {
       libs = (() => {
         const { hostname, search } = location || window.location;
-        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
+
+        // Use your repository's base URL for libs
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
-        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
+        return `https://main--lehre-site--berufsbildung-basel.hlx.page/libs`;
       })();
+
+      console.log(`Final libs path: ${libs}`);
       return libs;
-    }, () => libs,
+    },
+    () => libs,
   ];
 })();
 
