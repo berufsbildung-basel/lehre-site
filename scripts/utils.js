042535a5890024cae2a6f65640183d9924de4272
Tristan Thomas <tristan.thomas@bbzbl-it.ch>
2025-01-14 15:29:13 +0100

    nothing

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,7 +13,7 @@
 /**
  * The decision engine for where to get Milo's libs from.
  */
- export const [setLibs, getLibs] = (() => {
+export const [setLibs, getLibs] = (() => {
   let libs;
   return [
     (prodLibs, location) => {
@@ -22,16 +22,10 @@
         if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
-        if (hostname.includes('lehre-site--berufsbildung-basel')) {
-          return `https://${branch}--lehre-site--berufsbildung-basel.hlx.page/libs`;
-        }
-        return branch.includes('--')
-          ? `https://${branch}.hlx.live/libs`
-          : `https://${branch}--milo--adobecom.hlx.live/libs`;
+        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
       })();
       return libs;
-    },
-    () => libs,
+    }, () => libs,
   ];
 })();
 
