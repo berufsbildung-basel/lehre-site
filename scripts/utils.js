b7836a8fe6f6027f8d331a0e5069f5ed9a1ac242
Tristan Thomas <tristant@adobe.com>
2025-02-26 09:45:36 +0100

    added a comment

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,6 +13,23 @@
 /**
  * The decision engine for where to get Milo's libs from.
  */
+
+// export const [setLibs, getLibs] = (() => {
+//   let libs;
+//   return [
+//     (prodLibs, location) => {
+//       libs = (() => {
+//         const { hostname, search } = location || window.location;
+//         if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
+//         const branch = new URLSearchParams(search).get('milolibs') || 'main';
+//         if (branch === 'local') return 'http://localhost:6456/libs';
+//         return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--apprentice-milo--t0risutan.hlx.live/libs`;
+//       })();
+//       return libs;
+//     }, () => libs,
+//   ];
+// })();
+
 export const [setLibs, getLibs] = (() => {
   let libs;
   return [
